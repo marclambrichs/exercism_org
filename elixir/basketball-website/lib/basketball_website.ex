62532966067c774367611044cb166fa1632db267
Marc Lambrichs <marc.lambrichs@gmail.com>
2023-06-16 08:09:14 +0200

    Add basketball-website, second iteration

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,13 +1,7 @@
 defmodule BasketballWebsite do
   @spec extract_from_path(map(), String.t()) :: String.t() | nil
-  def extract_from_path(data, str), do: extract_from_list(data, String.split(str, "."))
-
-  @doc false
-  @spec extract_from_list(map(), list()) :: String.t() | nil
-  defp extract_from_list(data, []), do: data
-
-  defp extract_from_list(data, [h | t]) do
-    if data[h], do: extract_from_list(data[h], t), else: nil
+  def extract_from_path(data, str) do
+    Enum.reduce(String.split(str, "."), data, fn k, acc -> acc[k] end)
   end
 
   @spec get_in_path(map(), String.t()) :: String.t() | nil
