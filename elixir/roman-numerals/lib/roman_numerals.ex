e567ef38e32b7e38a0d80caa77e274323d2e009a
Marc Lambrichs <mlambrichs@melange-it.nl>
2023-11-13 07:16:40 +0100

    Use list of lists for roman numerals

--- elixir/roman-numerals/lib/roman_numerals.ex
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -2,39 +2,36 @@ defmodule RomanNumerals do
   @doc """
   Convert the number to a roman number.
   """
-  @roman_numerals %{
-    1 => "I",
-    4 => "IV",
-    5 => "V",
-    9 => "IX",
-    10 => "X",
-    40 => "XL",
-    50 => "L",
-    90 => "XC",
-    100 => "C",
-    400 => "CD",
-    500 => "D",
-    900 => "CM",
-    1000 => "M"
-  }
+  @roman_numerals [
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"]
+  ]
 
   @spec numeral(pos_integer) :: String.t()
   def numeral(number) do
     {_rest, string} =
       @roman_numerals
-      |> Map.keys()
-      |> Enum.sort()
-      |> Enum.reverse()
       |> List.foldl({number, ""}, &calculate/2)
 
     string
   end
 
-  defp calculate(roman_value, {number, string}) do
-    {rem(number, roman_value), string <> string_by_key(number, roman_value)}
+  defp calculate([roman_value, symbol], {number, string}) do
+    {rem(number, roman_value), string <> string_by_key(number, roman_value, symbol)}
   end
 
-  defp string_by_key(number, key) do
-    String.duplicate(@roman_numerals[key], div(number, key))
+  defp string_by_key(number, key, symbol) do
+    String.duplicate(symbol, div(number, key))
   end
 end
